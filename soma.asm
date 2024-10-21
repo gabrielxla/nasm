b01a86e366e3130c161e34af4c1c518658ade502
Gabriel Lima <159468890+gabrielxla@users.noreply.github.com>
2024-10-20 23:29:01 -0300

    Update soma.asm

--- soma.asm
+++ soma.asm
@@ -8,8 +8,8 @@ section .text
 
 _start:
     mov rax, [num1]     ;move o valor de num1 para rax
-    mov rax, [num2]     ;adiciona o valor de um num2 a rax
-    mov [result], rax   ;armazena o resultado em result
+    add rax, [num2]     ;adiciona o valor de um num2 a rax
+    add [result], rax   ;armazena o resultado em result
 
     mov rax, 60         ;sys_exit system call
     xor rdi, rdi        ;exit code 0
